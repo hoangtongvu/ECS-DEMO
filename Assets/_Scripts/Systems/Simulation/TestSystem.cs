686d8f90a30413fc6853e5a12970df918c06ddd7
hoangtongvu <hoangtongvu@gmail.com>
2024-02-17 22:50:39 +0700

    Add a system that disable n entities per Enter key down.
    - Disable here mean set EnableableTag = false.

---
+++ Assets/_Scripts/Systems/Simulation/TestSystem.cs
@@ -0,0 +1,39 @@
+using Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Systems.Simulation
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public partial class TestSystem : SystemBase
+    {
+        private int index = 0;
+        private int amountPerTime = 1000;
+        private DynamicBuffer<EntityRefElement> entities;
+
+        protected override void OnCreate()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!SystemAPI.TryGetSingletonBuffer(out this.entities)) return;
+            if (this.index >= 10) return;
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                //Disable Tag for {amountPerTime} entities.
+                UnityEngine.Debug.Log($"Disabled {(this.index + 1) * this.amountPerTime} Entities.");
+                for (int i = this.index; i < (this.index + 1) * this.amountPerTime; i++)
+                {
+                    Entity e = entities[i].entity;
+                    SystemAPI.SetComponentEnabled<EnableableTag>(e, false);
+                }
+                this.index++;
+            }
+
+        }
+    }
+}
