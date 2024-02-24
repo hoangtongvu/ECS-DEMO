851adb524ce68737dfc43f77cc372348392dee45
hoangtongvu <hoangtongvu@gmail.com>
2024-02-24 16:25:58 +0700

    Add PlayerTag.

---
+++ Assets/_Scripts/Authoring/Player/PlayerTagAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Components.Player;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring.Player
+{
+    public class PlayerTagAuthoring : MonoBehaviour
+    {
+
+        private class Baker : Baker<PlayerTagAuthoring>
+        {
+            public override void Bake(PlayerTagAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+                AddComponent<PlayerTag>(entity);
+
+            }
+        }
+    }
+}
