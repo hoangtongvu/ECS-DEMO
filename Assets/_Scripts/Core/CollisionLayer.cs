a5e81d707794bccfd3d3af7c9614251b9ec677d5
hoangtongvu <hoangtongvu@gmail.com>
2024-03-24 22:27:32 +0700

    Add CollisionLayer enum.
    - Add Player Layer.

---
+++ Assets/_Scripts/Core/CollisionLayer.cs
@@ -0,0 +1,11 @@
+
+
+namespace Core
+{
+    public enum CollisionLayer // TODO: Auto gen CollisionLayer based on TagManager.asset
+    {
+        Default = 1 << 0,
+        Player = 1 << 6,
+
+    }
+}
