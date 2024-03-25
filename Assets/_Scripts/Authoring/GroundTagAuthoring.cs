e7e6d40e5998ce918b75b47579f5967fbdf11fc7
hoangtongvu <hoangtongvu@gmail.com>
2024-03-25 22:15:10 +0700

    Create GroundTag.

---
+++ Assets/_Scripts/Authoring/GroundTagAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Components.Tag;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring
+{
+    public class GroundTagAuthoring : MonoBehaviour
+    {
+
+        private class Baker : Baker<GroundTagAuthoring>
+        {
+            public override void Bake(GroundTagAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+                AddComponent<GroundTag>(entity);
+
+            }
+        }
+    }
+}
