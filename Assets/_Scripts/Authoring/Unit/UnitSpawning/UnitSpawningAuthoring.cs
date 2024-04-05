f3fd618bbab4a25e0b5119ea72e998606f4025fc
hoangtongvu <hoangtongvu@gmail.com>
2024-04-05 16:14:47 +0700

    Add SpawningComponent, UnitProfileSO.

---
+++ Assets/_Scripts/Authoring/Unit/UnitSpawning/UnitSpawningAuthoring.cs
@@ -0,0 +1,58 @@
+﻿using Components.Unit.UnitSpawning;
+using Core.Unit;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring.Unit.UnitSpawning
+{
+    public class UnitSpawningAuthoring : MonoBehaviour
+    {
+        public UnitProfileSO UnitProfileSO;
+        public bool CanSpawn = false;
+        public int SpawnCount = 0;
+        public float SpawnRadius = 3f;
+
+
+        private class Baker : Baker<UnitSpawningAuthoring>
+        {
+            public override void Bake(UnitSpawningAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic); // Dynamic or static?? any dynamic unit can spawn?
+
+                AddComponent(entity, new PrefabToSpawn
+                {
+                    Value = GetEntity(authoring.UnitProfileSO.Prefab, TransformUsageFlags.Dynamic),
+                });
+
+                AddComponent(entity, new CanSpawnState
+                {
+                    Value = authoring.CanSpawn,
+                });
+
+                AddComponent(entity, new SpawnCount
+                {
+                    Value = authoring.SpawnCount,
+                });
+
+                AddComponent(entity, new SpawnRadius
+                {
+                    Value = authoring.SpawnRadius,
+                });
+
+                AddComponent(entity, new SpawnDuration
+                {
+                    DurationPerSpawn = authoring.UnitProfileSO.DurationPerUnit,
+                    DurationCounterSecond = 0,
+                });
+
+
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, this.SpawnRadius);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/Components/Unit/UnitSpawning/SpawningComponent.cs
@@ -0,0 +1,33 @@
+using Unity.Entities;
+
+namespace Components.Unit.UnitSpawning
+{
+    public struct PrefabToSpawn : IComponentData
+    {
+        public Entity Value;
+    }
+
+    public struct CanSpawnState : IComponentData
+    {
+        public bool Value;
+    }
+    
+    public struct SpawnCount : IComponentData
+    {
+        public int Value;
+    }
+    
+    public struct SpawnRadius : IComponentData
+    {
+        public float Value;
+    }
+
+    public struct SpawnDuration : IComponentData
+    {
+        public float DurationPerSpawn;
+        public float DurationCounterSecond;
+    }
+
+
+
+}
