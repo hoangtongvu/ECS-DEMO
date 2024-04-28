3182273f6fec6fb50ad46a703eb209c741bbd7fc
hoangtongvu <hoangtongvu@gmail.com>
2024-04-28 10:10:27 +0700

    Update SingletonUtilities.

--- Assets/_Scripts/Core/SingletonUtilities.cs
+++ Assets/_Scripts/Core/SingletonUtilities.cs
@@ -4,72 +4,18 @@ using UnityEngine;
 
 namespace Core
 {
-    //public static class SingletonUtilities
-    //{
-    //    private static Entity singletonEntity;
-
-    //    public static void Setup(EntityManager em)
-    //    {
-    //        if (em.Exists(singletonEntity))
-    //        {
-    //            UnityEngine.Debug.LogWarning("Default singleton already created");
-    //            return;
-    //        }
-
-    //        singletonEntity = em.CreateEntity(typeof(DefaultSingletonTag));
-    //        em.SetName(singletonEntity, "*DefaultSingletonEntity");
-    //    }
-
-    //    public static Entity GetDefaultSingletonEntity(this EntityManager em) => singletonEntity;
-
-    //    public static bool HasSingleton<T>(this EntityManager em) where T : IComponentData
-    //    {
-    //        return em.HasComponent<T>(singletonEntity);
-    //    }
-
-    //    public static Entity CreateOrAddSingletonICD<T>(this EntityManager em) where T : IComponentData
-    //    {
-    //        if (!em.HasComponent<T>(singletonEntity))
-    //            em.AddComponent<T>(singletonEntity);
-
-    //        return singletonEntity;
-    //    }
-
-    //    public static Entity CreateOrSetSingletonICD<T>(this EntityManager em, T data) where T : unmanaged, IComponentData
-    //    {
-    //        if (em.HasComponent<T>(singletonEntity))
-    //            em.SetComponentData(singletonEntity, data);
-    //        else
-    //            em.AddComponentData(singletonEntity, data);
-
-    //        return singletonEntity;
-    //    }
-
-    //    public static Entity CreateOrAddSingletonBuffer<T>(this EntityManager em) where T : unmanaged, IBufferElementData
-    //    {
-    //        if (!em.HasBuffer<T>(singletonEntity))
-    //            em.AddBuffer<T>(singletonEntity);
-
-    //        return singletonEntity;
-    //    }
-
-    //    public static T GetSingleton<T>(this EntityManager em) where T : unmanaged, IComponentData
-    //    {
-    //        return em.GetComponentData<T>(singletonEntity);
-    //    }
-
-
-    //}
-
+    
     public struct SingletonUtilities
     {
 
-        private static Entity singletonEntity;
+        private Entity singletonEntity;
         private static SingletonUtilities instance;
         private static bool isValid;
 
         private EntityManager entityManager;
 
+        public EntityManager EntityManager => entityManager;
+        public Entity DefaultSingletonEntity => singletonEntity;
 
 #if UNITY_EDITOR
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
@@ -80,8 +26,8 @@ namespace Core
 
         private SingletonUtilities(EntityManager em)
         {
-            singletonEntity = em.CreateEntity(typeof(DefaultSingletonTag));
-            em.SetName(singletonEntity, "*DefaultSingletonEntity");
+            this.singletonEntity = em.CreateEntity(typeof(DefaultSingletonTag));
+            em.SetName(this.singletonEntity, "*DefaultSingletonEntity");
 
             this.entityManager = em;
         }
@@ -102,36 +48,55 @@ namespace Core
 
         private static void DestroyInstance() => isValid = false;
 
-        public Entity GetDefaultSingletonEntity() => singletonEntity;
 
-        public bool HasSingleton<T>() where T : IComponentData => this.entityManager.HasComponent<T>(singletonEntity);
 
-        public Entity CreateOrAddSingletonICD<T>() where T : IComponentData
+        public bool HasSingleton<T>() where T : IComponentData => this.entityManager.HasComponent<T>(this.singletonEntity);
+
+        public T GetSingleton<T>() where T : unmanaged, IComponentData => this.entityManager.GetComponentData<T>(this.singletonEntity);
+
+
+        public Entity AddBuffer<T>() where T : unmanaged, IBufferElementData
         {
-            if (!this.entityManager.HasComponent<T>(singletonEntity))
-                this.entityManager.AddComponent<T>(singletonEntity);
-
-            return singletonEntity;
+            if (!this.entityManager.HasBuffer<T>(this.singletonEntity))
+                this.entityManager.AddBuffer<T>(this.singletonEntity);
+            return this.singletonEntity;
         }
 
-        public Entity CreateOrSetSingletonICD<T>(T data) where T : unmanaged, IComponentData
+        public Entity AddOrSetComponentData<T>(T data) where T : unmanaged, IComponentData
         {
-            if (this.entityManager.HasComponent<T>(singletonEntity))
-                this.entityManager.SetComponentData(singletonEntity, data);
+            if (this.entityManager.HasComponent<T>(this.singletonEntity))
+                this.entityManager.SetComponentData(this.singletonEntity, data);
             else
-                this.entityManager.AddComponentData(singletonEntity, data);
+                this.entityManager.AddComponentData(this.singletonEntity, data);
 
-            return singletonEntity;
+            return this.singletonEntity;
         }
 
-        public Entity CreateOrAddSingletonBuffer<T>() where T : unmanaged, IBufferElementData
+        public Entity AddComponent<T>() where T : IComponentData
         {
-            if (!this.entityManager.HasBuffer<T>(singletonEntity))
-                this.entityManager.AddBuffer<T>(singletonEntity);
+            if (!this.entityManager.HasComponent<T>(this.singletonEntity))
+                this.entityManager.AddComponent<T>(this.singletonEntity);
+
+            return this.singletonEntity;
+        }
+
+    }
+
+    public static class SingletonUtilitiesManagedExtensions
+    {
+        public static Entity AddOrSetComponentData<T>(this SingletonUtilities su, T data) where T : class, IComponentData, new()
+        {
+            EntityManager em = su.EntityManager;
+            Entity singletonEntity = su.DefaultSingletonEntity;
+
+            if (em.HasComponent<T>(singletonEntity))
+                em.SetComponentData(singletonEntity, data);
+            else
+                em.AddComponentData(singletonEntity, data);
+
             return singletonEntity;
         }
 
-        public T GetSingleton<T>() where T : unmanaged, IComponentData => this.entityManager.GetComponentData<T>(singletonEntity);
 
     }
 }
