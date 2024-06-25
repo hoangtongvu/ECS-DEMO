cdd7d74afbdbb7aa7567bf418759d8580a9ea3f2
hoangtongvu <hoangtongvu@gmail.com>
2024-06-25 22:15:24 +0700

    Refactor UIRegister, Add UIPrefab
    - Remove old UI registering stuff in scene.
    - Now loading UI via Prefab

--- Assets/_Scripts/Core/UIPoolMapRegister.cs
+++ Assets/_Scripts/Core/UIPoolMapRegister.cs
@@ -2,7 +2,6 @@ using Core.MyEvent.PubSub.Messages;
 using Core.MyEvent.PubSub.Messengers;
 using Core.UI;
 using Core.UI.Identification;
-using System.Collections.Generic;
 using UnityEngine;
 using ZBase.Foundation.PubSub;
 
@@ -12,37 +11,46 @@ namespace Core
     public class UIPoolMapRegister : MonoBehaviour
     {
 
-        [System.Serializable]
-        private class RegisterValue
-        {
-            public UIType Type;
-            public GameObject Prefab;
-            public ObjPool<BaseUICtrl> UIPool;
-        }
-
-
-        [SerializeField] private List<RegisterValue> registerValues;
+        [SerializeField] private BaseUICtrl[] uiCtrls;
 
 
         private void Awake()
         {
-            if (this.registerValues.Count == 0) return;
+            this.LoadAllUIPrefabs();
             this.Register();
         }
 
         private void Register()
         {
-            foreach (var value in this.registerValues)
+            foreach (var uiCtrl in this.uiCtrls)
             {
+                UIType type = uiCtrl.UIID.Type;
+
+                this.InstantiateObjPool(type, out var objPool);
+
                 MapRegisterMessenger.MessagePublisher.Publish(new UIPoolRegisterMessage
                 {
-                    Type = value.Type,
-                    Prefab = value.Prefab,
-                    UIPool = value.UIPool,
+                    Type = type,
+                    Prefab = uiCtrl.gameObject,
+                    UIPool = objPool,
                 });
             }
         }
 
 
+        private void LoadAllUIPrefabs() => this.uiCtrls = Resources.LoadAll<BaseUICtrl>("UI");
+
+        private void InstantiateObjPool(UIType type, out BaseUIPool objPool)
+        {
+            GameObject newGameObject = new($"{type} Pool");
+            newGameObject.transform.SetParent(this.transform);
+            objPool = newGameObject.AddComponent<BaseUIPool>();
+        }
+
+        // Load UI Prefabs from Resources.
+        // Instantiate objPool for each UIPrefab.
+        // Set objPool's parent to ObjPoolsHolder.
+        // With each prefab, GetComponent BaseUICtrl and get UIType from that.
+        // Publish Message.
     }
 }
