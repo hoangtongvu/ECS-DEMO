3f88a3bc9813c004657dba0cb356798669778c40
hoangtongvu <hoangtongvu@gmail.com>
2024-06-28 16:45:59 +0700

    Add deduct wallet on spawn unit.
    - Add [BurstDiscard] for SubscribeEvent in IncSpawnCountSystem to remove burst error.
    - Add IdMatched() for clarity.

--- Assets/_Scripts/Systems/Simulation/Unit/UnitSpawning/IncSpawnCountSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Unit/UnitSpawning/IncSpawnCountSystem.cs
@@ -6,6 +6,10 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using ZBase.Foundation.PubSub;
+using Core.UI.Identification;
+using Components.GameResource;
+using Core.Unit;
+using Core.GameResource;
 
 namespace Systems.Simulation.Unit.UnitSpawning
 {
@@ -21,13 +25,17 @@ namespace Systems.Simulation.Unit.UnitSpawning
         public void OnCreate(ref SystemState state)
         {
             this.spawnUnitMessages = new(Allocator.Persistent);
-            GameplayMessenger.MessageSubscriber.Subscribe<SpawnUnitMessage>(this.SpawnUnitMessageHandle);
+            this.SubscribeEvents();
         }
 
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            // Only handle Player's side this time.
+            var resourceWallet = SystemAPI.GetSingletonBuffer<ResourceWalletElement>();
+            var unitCostMap = SystemAPI.GetSingleton<UnitCostMap>();
+
             // Put foreach inside while loop is more efficient in this situation.
             while (this.spawnUnitMessages.TryDequeue(out var message))
             {
@@ -48,15 +56,58 @@ namespace Systems.Simulation.Unit.UnitSpawning
                             continue;
                         }
 
-                        if (!message.ProfileID.Equals(profile.UIID.Value)) continue;
+                        if (!this.IdMatched(message.ProfileID, profile.UIID.Value)) continue;
+                        if (!this.HaveEnoughResources(resourceWallet, in unitCostMap, in profile, out var walletArr)) continue;
+
+                        resourceWallet.CopyFrom(walletArr);
                         profile.SpawnCount++;
                     }
                 }
             }
         }
 
+        [BurstDiscard]
+        private void SubscribeEvents()
+        {
+            GameplayMessenger.MessageSubscriber.Subscribe<SpawnUnitMessage>(this.SpawnUnitMessageHandle);
+        }
+
         private void SpawnUnitMessageHandle(SpawnUnitMessage spawnUnitMessage) => this.spawnUnitMessages.Enqueue(spawnUnitMessage);
 
+        private bool IdMatched(UIID first, UIID second) => first.Equals(second);
+
+        private bool HaveEnoughResources(
+            DynamicBuffer<ResourceWalletElement> resourceWallet
+            , in UnitCostMap unitCostMap
+            , in UnitSpawningProfileElement profile
+            , out NativeArray<ResourceWalletElement> walletArr)
+        {
+
+            walletArr = resourceWallet.ToNativeArray(Allocator.Temp);
+
+            int length = walletArr.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                ResourceType resourceType = (ResourceType)i;
+
+                var unitCostId = new UnitCostId(profile.UnitType, resourceType, profile.LocalIndex);
+
+                if (!unitCostMap.Value.TryGetValue(unitCostId, out uint cost)) continue;
+                long tempValue = (long) walletArr[i].Quantity - cost;
+                UnityEngine.Debug.Log($"{resourceType} {tempValue} = {walletArr[i].Quantity} - {cost}");
+
+                if (tempValue < 0) return false;
+                walletArr[i] = new ResourceWalletElement
+                {
+                    Type = resourceType,
+                    Quantity = (uint)tempValue,
+                };
+
+            }
+
+            return true;
+        }
 
 
     }
