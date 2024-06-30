1e62617cd2c9086b9d9d7b8a56c8c45bb7b98fdc
hoangtongvu <hoangtongvu@gmail.com>
2024-06-30 15:51:31 +0700

    Add ResourceDisplayMessage.

---
+++ Assets/_Scripts/Core/GameResource/ResourceDisplayMessage.cs
@@ -0,0 +1,8 @@
+﻿using Core.GameResource;
+using ZBase.Foundation.PubSub;
+
+namespace Core.MyEvent.PubSub.Messages
+{
+    public readonly record struct ResourceDisplayMessage(ResourceType ResourceType, uint Quantity) : IMessage;
+
+}
