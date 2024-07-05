68af6ddf67ed7e3993ddf0840ff20168b62fb3be
hoangtongvu <hoangtongvu@gmail.com>
2024-07-05 21:28:13 +0700

    Add UnitToolHolder

---
+++ Assets/_Scripts/Authoring/Unit/UnitToolHolderAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Components.Unit;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring.Unit
+{
+    public class UnitToolHolderAuthoring : MonoBehaviour
+    {
+
+        private class Baker : Baker<UnitToolHolderAuthoring>
+        {
+            public override void Bake(UnitToolHolderAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+                AddComponent(entity, new UnitToolHolder
+                {
+                    Value = Entity.Null,
+                });
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Components/Unit/UnitToolHolder.cs
@@ -0,0 +1,10 @@
+using Unity.Entities;
+
+namespace Components.Unit
+{
+    public struct UnitToolHolder : IComponentData
+    {
+        public Entity Value;
+    }
+
+}
