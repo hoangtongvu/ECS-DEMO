4e47d79ed9c4ed33c6551ddc813a3b87600d8c71
hoangtongvu <hoangtongvu@gmail.com>
2024-07-12 22:53:19 +0700

    Add ToolCallerSystem

---
+++ Assets/_Scripts/Systems/Simulation/Tool/ToolCallerSystem.cs
@@ -0,0 +1,110 @@
+using Unity.Entities;
+using Unity.Burst;
+using Components.Tool;
+using Components;
+using Components.Unit;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Systems.Simulation.Unit;
+
+namespace Systems.Simulation.Tool
+{
+
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateBefore(typeof(SetCurrentDisToTargetSystem))]
+    [BurstCompile]
+    public partial struct ToolCallerSystem : ISystem
+    {
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            var query0 = SystemAPI.QueryBuilder()
+                .WithAll<
+                    ToolHolderElement
+                    , LocalTransform
+                    , ToolCallerRadius
+                    , ToolPickRadius>()
+                .Build();
+
+            var query1 = SystemAPI.QueryBuilder()
+                .WithAll<
+                    UnitToolHolder
+                    , LocalTransform
+                    , TargetPosition
+                    , MoveAffecterICD
+                    , DistanceToTarget>()
+                .Build();
+
+            state.RequireForUpdate(query0);
+            state.RequireForUpdate(query1);
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+
+            foreach (var (toolsHolder, toolHolderTransformRef, toolCallerRadiusRef, toolPickRadiusRef) in
+                SystemAPI.Query<
+                    DynamicBuffer<ToolHolderElement>
+                    , RefRO<LocalTransform>
+                    , RefRO<ToolCallerRadius>
+                    , RefRO<ToolPickRadius>>())
+            {
+                if (toolsHolder.IsEmpty) continue;
+                int toolsHolderLength = toolsHolder.Length;
+
+                for (int i = 0; i < toolsHolderLength; i++)
+                {
+                    ref var toolHolder = ref toolsHolder.ElementAt(i);
+
+                    // May be adding a Jobless Tag to Unit more efficiently.
+
+                    foreach (var (unitToolHolderRef, unitTransformRef, targetPosRef, moveAffecterRef, distanceToTargetRef, unitEntity) in
+                        SystemAPI.Query<
+                            RefRW<UnitToolHolder>
+                            , RefRO<LocalTransform>
+                            , RefRW<TargetPosition>
+                            , RefRW<MoveAffecterICD>
+                            , RefRW<DistanceToTarget>>()
+                            .WithEntityAccess())
+                    {
+                        if (unitToolHolderRef.ValueRO.Value != Entity.Null) continue;
+                        // Even if we can set min distance then how we can make unit pick up tool upon stop.
+
+
+                        // calculate distance.
+                        // compare to range.
+                        float distance = math.distance(toolHolderTransformRef.ValueRO.Position, unitTransformRef.ValueRO.Position);
+                        if (distance > toolCallerRadiusRef.ValueRO.Value) continue;
+
+                        if (distance <= toolPickRadiusRef.ValueRO.Value)
+                        {
+                            // Pick the tool
+                            unitToolHolderRef.ValueRW.Value = toolHolder.Value;
+                            toolsHolder.RemoveAt(i);
+                            continue;
+                        }
+
+                        moveAffecterRef.ValueRW.Value = Core.Unit.MoveAffecter.Others;
+
+                        targetPosRef.ValueRW.Value = toolHolderTransformRef.ValueRO.Position;
+                        distanceToTargetRef.ValueRW.MinDistance = toolPickRadiusRef.ValueRO.Value;
+                        SystemAPI.SetComponentEnabled<MoveableState>(unitEntity, true);
+
+                    }
+
+                    break; // process tool one by one.
+
+                }
+
+            }
+
+
+        }
+
+        
+
+
+    }
+}
