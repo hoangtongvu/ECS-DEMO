36b62b262b3d4118fe266d973ffc91aa0715c8ea
hoangtongvu <hoangtongvu@gmail.com>
2024-07-14 21:53:04 +0700

    rename System

--- Assets/_Scripts/Systems/Simulation/Tool/ToolHoldCountAddSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Tool/ToolHoldCountAddSystem.cs
@@ -10,7 +10,7 @@ namespace Systems.Simulation.Tool
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     [UpdateAfter(typeof(SpawnPrefabSystem))]
     [BurstCompile]
-    public partial struct ToolHolderAddSystem : ISystem
+    public partial struct ToolHoldCountAddSystem : ISystem
     {
 
         [BurstCompile]
