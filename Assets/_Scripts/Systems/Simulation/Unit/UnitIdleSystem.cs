a07a8193eb2f92c3eb2a51c7677e68422e8f05af
hoangtongvu <hoangtongvu@gmail.com>
2024-10-28 12:11:04 +0700

    Update systems to adapt new Reaction method

--- Assets/_Scripts/Systems/Simulation/Unit/UnitIdleSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Unit/UnitIdleSystem.cs
@@ -2,11 +2,14 @@ using Unity.Entities;
 using Unity.Burst;
 using Components;
 using Components.Unit;
+using Components.Damage;
+using Utilities.Extensions;
+using Components.Unit.Reaction;
 
 namespace Systems.Simulation.Unit
 {
 
-    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
     [BurstCompile]
     public partial struct UnitIdleSystem : ISystem
     {
@@ -14,27 +17,60 @@ namespace Systems.Simulation.Unit
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
+            var query0 = SystemAPI.QueryBuilder()
+                .WithAll<
+                    IdleStartedTag
+                    , IsAliveTag
+                    , CanMoveEntityTag
+                    , IsUnitWorkingTag
+                    , UnitIdleTimeCounter
+                    , AnimatorData>()
+                .Build();
+
+            state.RequireForUpdate(query0);
+
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            foreach (var timeCounterRef in
-                SystemAPI.Query<
-                    RefRW<UnitIdleTimeCounter>>()
-                    .WithAny<
-                        CanMoveEntityTag
-                        , IsUnitWorkingTag>())
-            {
-                timeCounterRef.ValueRW.Value = 0;
-            }
 
-            //foreach (var unitReactionRef in
-            //    SystemAPI.Query<>()
-            //        .WithAll<IsAliveTag>()
-            //        .WithDisabled<CanMoveEntityTag>())
-            //{
-            //}
+            foreach (var (idleStartedTag, isAliveTag, canMoveEntityTag, isUnitWorkingTag, timeCounterRef, animatorDataRef) in
+                SystemAPI.Query<
+                    EnabledRefRW<IdleStartedTag>
+                    , EnabledRefRO<IsAliveTag>
+                    , EnabledRefRO<CanMoveEntityTag>
+                    , EnabledRefRO<IsUnitWorkingTag>
+                    , RefRW<UnitIdleTimeCounter>
+                    , RefRW<AnimatorData>>()
+                    .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState))
+            {
+                bool reactionStarted = idleStartedTag.ValueRO;
+                bool canUpdate = isAliveTag.ValueRO && !canMoveEntityTag.ValueRO && !isUnitWorkingTag.ValueRO;
+
+                if (canUpdate)
+                {
+                    if (!reactionStarted)
+                    {
+                        //OnReactionStart();
+                        animatorDataRef.ValueRW.Value.ChangeValue("Idle");
+                        timeCounterRef.ValueRW.Value = 0;
+                        idleStartedTag.ValueRW = true;
+                    }
+
+                    //OnReactionUpdate();
+                    timeCounterRef.ValueRW.Value += SystemAPI.Time.DeltaTime;
+
+                }
+
+                if (!canUpdate && reactionStarted)
+                {
+                    //OnReactionEnd();
+                    idleStartedTag.ValueRW = false;
+                }
+
+
+            }
 
 
         }
--- Assets/_Scripts/Systems/Simulation/Unit/UnitPatrolSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Unit/UnitPatrolSystem.cs
@@ -4,6 +4,13 @@ using Components;
 using Components.Damage;
 using Components.Unit;
 using Components.Misc.GlobalConfigs;
+using Components.Unit.MyMoveCommand;
+using Core.Unit.MyMoveCommand;
+using Unity.Mathematics;
+using Utilities.Helpers;
+using Unity.Collections;
+using Components.MyEntity;
+using Unity.Transforms;
 
 namespace Systems.Simulation.Unit
 {
@@ -12,50 +19,128 @@ namespace Systems.Simulation.Unit
     [BurstCompile]
     public partial struct UnitPatrolSystem : ISystem
     {
+        private Random rand;
 
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
+            this.rand = new(1);
+
+            var query0 = SystemAPI.QueryBuilder()
+                .WithAll<
+                    IsAliveTag
+                    , IsUnitWorkingTag
+                    , UnitIdleTimeCounter
+                    , AnimatorData
+                    , NeedsInitWalkTag>()
+                .Build();
+
+            state.RequireForUpdate(query0);
+
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var gameGlobalConfigs = SystemAPI.GetSingleton<GameGlobalConfigsICD>();
+            var moveCommandSourceMap = SystemAPI.GetSingleton<MoveCommandSourceMap>();
 
-            foreach (var (idleTimeCounterRef, canMoveEntityTag, entity) in
+            foreach (var (idleTimeCounterRef, needInitWalkTag) in
                 SystemAPI.Query<
                     RefRW<UnitIdleTimeCounter>
-                    , EnabledRefRW<CanMoveEntityTag>>()
+                    , EnabledRefRW<NeedsInitWalkTag>>()
                     .WithAll<IsAliveTag>()
                     .WithDisabled<IsUnitWorkingTag>()
-                    .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)
-                    .WithEntityAccess())
+                    .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState))
             {
-                // Count the timer for idle state
-                // On Idle end -> Make Unit to walk to a random position around
-                // No MoveCommand left -> auto change CanMoveEntityTag disabled -> Idle state start again
-
-
-                if (!canMoveEntityTag.ValueRO)
-                {
-                    // Also counter = 0??
-                    idleTimeCounterRef.ValueRW.Value += SystemAPI.Time.DeltaTime;
-                }
-
-
                 if (idleTimeCounterRef.ValueRO.Value >= gameGlobalConfigs.Value.UnitIdleMaxDuration)
                 {
                     idleTimeCounterRef.ValueRW.Value = 0;
-                    SystemAPI.SetComponentEnabled<NeedsInitWalkTag>(entity, true);
+                    needInitWalkTag.ValueRW = true;
                 }
 
             }
 
+            state.Dependency = new SetPatrolJob
+            {
+                moveCommandSourceMap = moveCommandSourceMap.Value,
+                Rand = new Random(this.rand.NextUInt(1, 15)),
+                UnitWalkMinDistance = gameGlobalConfigs.Value.UnitWalkMinDistance,
+                UnitWalkMaxDistance = gameGlobalConfigs.Value.UnitWalkMaxDistance,
+                UnitWalkSpeed = gameGlobalConfigs.Value.UnitWalkSpeed,
+            }.ScheduleParallel(state.Dependency);
 
         }
 
 
+        [WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)]
+        [BurstCompile]
+        private partial struct SetPatrolJob : IJobEntity
+        {
+            [ReadOnly] public NativeHashMap<MoveCommandSourceId, byte> moveCommandSourceMap;
+            [ReadOnly] public Random Rand;
+            [ReadOnly] public float UnitWalkMinDistance;
+            [ReadOnly] public float UnitWalkMaxDistance;
+            [ReadOnly] public float UnitWalkSpeed;
+
+
+            [BurstCompile]
+            void Execute(
+                EnabledRefRW<NeedsInitWalkTag> needInitWalkTag
+                , in UnitId unitId
+                , ref MoveCommandElement moveCommandElement
+                , ref InteractingEntity interactingEntity
+                , ref InteractionTypeICD interactionTypeICD
+                , in LocalTransform transform
+                , ref MoveSpeedLinear moveSpeed
+                , ref TargetPosition targetPosition
+                , EnabledRefRW<TargetPosChangedTag> targetPosChangedTag
+                , EnabledRefRW<CanMoveEntityTag> canMoveEntityTag
+                , ref AnimatorData animatorData)
+            {
+                if (!needInitWalkTag.ValueRO) return;
+                needInitWalkTag.ValueRW = false;
+
+                bool canOverrideCommand =
+                    MoveCommandHelper.TryOverrideMoveCommand(
+                        in moveCommandSourceMap
+                        , unitId.UnitType
+                        , ref moveCommandElement
+                        , ref interactingEntity
+                        , ref interactionTypeICD
+                        , MoveCommandSource.PlayerCommand
+                        , unitId.LocalIndex);
+
+                if (!canOverrideCommand) return;
+
+                // Get a random direction
+                float2 randomDir = this.Rand.NextFloat2Direction();
+
+                // Generate a random distance between the min and max distance
+                float randomDis = this.Rand.NextFloat(this.UnitWalkMinDistance, this.UnitWalkMaxDistance);
+
+                float2 tempVector2 = randomDir * randomDis;
+
+                // Calculate the random target position
+                float3 randomPoint = transform.Position + new float3(tempVector2.x, 0, tempVector2.y);
+
+                // Use this randomPoint for the move command or other logic
+                moveCommandElement.Float3 = randomPoint;
+
+                // Set move speed
+                moveSpeed.Value = this.UnitWalkSpeed;
+
+                targetPosition.Value = randomPoint;
+                targetPosChangedTag.ValueRW = true;
+
+                canMoveEntityTag.ValueRW = true;
+
+                animatorData.Value.Value = "Walking_A";
+                animatorData.Value.ValueChanged = true;
+
+            }
+
+        }
 
 
     }
