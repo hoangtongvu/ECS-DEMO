a439d28a6b2c5a44968df64d01b10aaf49f35f92
hoangtongvu <hoangtongvu@gmail.com>
2025-02-06 22:27:03 +0700

    Add MapDebugConfigAuthoring

---
+++ Assets/_Scripts/Authoring/Misc/WorldMap/MapDebugConfigAuthoring.cs
@@ -0,0 +1,27 @@
+﻿using Components.Misc.WorldMap;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring.Misc.WorldMap
+{
+    public class GridDebugConfigAuthoring : MonoBehaviour
+    {
+        [SerializeField] private bool showCost = true;
+        [SerializeField] private bool showGridLines = true;
+
+        private class Baker : Baker<GridDebugConfigAuthoring>
+        {
+            public override void Bake(GridDebugConfigAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+                AddComponent(entity, new MapDebugConfig
+                {
+                    ShowCost = authoring.showCost,
+                    ShowGridLines = authoring.showGridLines,
+                });
+
+            }
+        }
+    }
+}
