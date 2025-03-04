7f5b36f5bfd68c4c0ff4587b433232610a8ed5be
hoangtongvu <hoangtongvu@gmail.com>
2025-03-04 14:10:11 +0700

    Update ChunkExitHelper + add tests

--- Assets/_Scripts/Utilities/Helpers/Misc/WorldMap/ChunkExitHelper.cs
+++ Assets/_Scripts/Utilities/Helpers/Misc/WorldMap/ChunkExitHelper.cs
@@ -33,11 +33,34 @@ namespace Utilities.Helpers.Misc.WorldMap
 
         }
 
-        /// <summary>
-        /// This function assumes that return cell is either FirstCell or SecondCell of exit.
-        /// </summary>
         [BurstCompile]
-        public static void GetUnsafeCellBelongToChunkFromExit(
+        public static void GetExitIndexesFromChunk(
+            in ChunkIndexToExitIndexesMap chunkIndexToExitIndexesMap
+            , in ChunkExitIndexesContainer chunkExitIndexesContainer
+            , int chunkIndex
+            , Allocator allocator
+            , out NativeArray<int> exitIndexes)
+        {
+            var indexRange = chunkIndexToExitIndexesMap.Value[chunkIndex];
+            int upperBound = indexRange.StartIndex + indexRange.Amount;
+
+            exitIndexes = new NativeArray<int>(indexRange.Amount, allocator);
+            int tempIndex = 0;
+
+            for (int i = indexRange.StartIndex; i < upperBound; i++)
+            {
+                exitIndexes[tempIndex] = chunkExitIndexesContainer.Value[i];
+                tempIndex++;
+            }
+
+        }
+
+        /// <summary>
+        /// This function assumes that the cell belong to chunk is either FirstCell or SecondCell of exit.
+        /// </summary>
+        /// <returns><c>true</c> if exit is in right order with input <paramref name="chunkIndex"/></returns>
+        [BurstCompile]
+        public static bool GetUnsafeInnerCellFromExit(
             in WorldTileCostMap costMap
             , in ChunkExit exit
             , int chunkIndex
@@ -49,11 +72,66 @@ namespace Utilities.Helpers.Misc.WorldMap
             {
                 cell = cell0;
                 cellMapIndex = exit.FirstCellMapIndex;
+                return true;
+            }
+
+            cell = costMap.Value[exit.SecondCellMapIndex];
+            cellMapIndex = exit.SecondCellMapIndex;
+            return false;
+
+        }
+
+        /// <summary>
+        /// This function assumes that the cell belong to chunk is either FirstCell or SecondCell of exit.
+        /// </summary>
+        [BurstCompile]
+        public static void GetUnsafeOuterCellFromExit(
+            in WorldTileCostMap costMap
+            , in ChunkExit exit
+            , int chunkIndex
+            , out Cell cell
+            , out int cellMapIndex)
+        {
+            Cell cell0 = costMap.Value[exit.FirstCellMapIndex];
+            if (cell0.ChunkIndex != chunkIndex)
+            {
+                cell = cell0;
+                cellMapIndex = exit.FirstCellMapIndex;
                 return;
             }
 
             cell = costMap.Value[exit.SecondCellMapIndex];
             cellMapIndex = exit.SecondCellMapIndex;
+
+        }
+
+        /// <summary>
+        /// This function assumes that the cell belong to chunk is either FirstCell or SecondCell of exit.
+        /// </summary>
+        /// <returns><c>true</c> if exit is in right order with input <paramref name="chunkIndex"/></returns>
+        [BurstCompile]
+        public static bool GetUnsafeCellsInRightOrder(
+            in WorldTileCostMap costMap
+            , in ChunkExit exit
+            , int chunkIndex
+            , out Cell innerCell
+            , out int innerCellMapIndex
+            , out Cell outerCell
+            , out int outerCellMapIndex)
+        {
+            Cell cell0 = costMap.Value[exit.FirstCellMapIndex];
+            Cell cell1 = costMap.Value[exit.SecondCellMapIndex];
+
+            if (cell0.ChunkIndex == chunkIndex)
+            {
+                (innerCell, innerCellMapIndex) = (cell0, exit.FirstCellMapIndex);
+                (outerCell, outerCellMapIndex) = (cell1, exit.SecondCellMapIndex);
+                return true;
+            }
+
+            (innerCell, innerCellMapIndex) = (cell1, exit.SecondCellMapIndex);
+            (outerCell, outerCellMapIndex) = (cell0, exit.FirstCellMapIndex);
+            return false;
 
         }
 
