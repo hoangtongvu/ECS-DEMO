092d0acce795514c30f59208e6a52587657d69e3
hoangtongvu <hoangtongvu@gmail.com>
2025-03-23 13:21:30 +0700

    Add InstantiateEntityOnBakeTag & BakeSystem

---
+++ Assets/_Scripts/Systems/Initialization/Misc/InstantiateEntityOnBakeSystem.cs
@@ -0,0 +1,41 @@
+using Components.Misc;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Systems.Initialization.Misc
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.BakingSystem)]
+    [BurstCompile]
+    public partial struct InstantiateEntityOnBakeSystem : ISystem
+    {
+        private EntityQuery query;
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            this.query = SystemAPI.QueryBuilder()
+                .WithAll<InstantiateEntityOnBakeTag>()
+                .WithAll<Prefab>()
+                .Build();
+
+            state.RequireForUpdate(query);
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var entities = this.query.ToEntityArray(Allocator.Temp);
+
+            int entityCount = entities.Length;
+
+            for (int i = 0; i < entityCount; i++)
+            {
+                state.EntityManager.RemoveComponent<Prefab>(entities[i]);
+            }
+
+        }
+
+    }
+
+}
