29ba25100916bdcd5f3a95924d22523cff5b0b4a
hoangtongvu <hoangtongvu@gmail.com>
2025-03-27 22:05:07 +0700

    Remove useless field in GameGlobalConfigs

--- Assets/_Scripts/Core/Misc/GlobalConfigs/GameGlobalConfigs.cs
+++ Assets/_Scripts/Core/Misc/GlobalConfigs/GameGlobalConfigs.cs
@@ -1,11 +1,9 @@
 namespace Core.Misc.GlobalConfigs
 {
-
     [System.Serializable]
     public struct GameGlobalConfigs
     {
         public float EntityMoveSpeedScale = 1f;
-        public float UnitStopMoveRadius = 1f;
         public float UnitInteractRadius = 1.5f;
 
         public float UnitWalkSpeed = 3f;
@@ -17,4 +15,5 @@ namespace Core.Misc.GlobalConfigs
 
         public GameGlobalConfigs() { }
     }
+
 }
