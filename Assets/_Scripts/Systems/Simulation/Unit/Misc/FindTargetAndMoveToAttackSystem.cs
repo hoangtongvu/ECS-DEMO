bb4405cb8bb987395c8514110ea8e25c17984fcb
hoangtongvu <hoangtongvu@gmail.com>
2025-06-02 08:01:03 +0700

    Comment out the attack faction logic for easier testing

--- Assets/_Scripts/Systems/Simulation/Unit/Misc/FindTargetAndMoveToAttackSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Unit/Misc/FindTargetAndMoveToAttackSystem.cs
@@ -210,9 +210,10 @@ namespace Systems.Simulation.Unit.Misc
                 {
                     var hit = hitList[i];
 
-                    this.FactionIndexLookup.TryGetComponent(hit.Entity, out var targetFactionIndex);
-                    if (targetFactionIndex == FactionIndex.Neutral) continue;
-                    if (factionIndex.Value == targetFactionIndex.Value) continue;
+                    // Note: The following logic works fine, just commented this out for testing purpose
+                    //this.FactionIndexLookup.TryGetComponent(hit.Entity, out var targetFactionIndex);
+                    //if (targetFactionIndex == FactionIndex.Neutral) continue;
+                    //if (factionIndex.Value == targetFactionIndex.Value) continue;
 
                     if (hit.Entity == unitEntity) continue;
                     if (smallestDistanceXZ <= hit.Distance) continue;
