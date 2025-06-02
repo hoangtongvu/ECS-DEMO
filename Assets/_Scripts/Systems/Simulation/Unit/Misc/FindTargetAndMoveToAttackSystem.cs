93d7f1bf2a93925812d19a05b37c56a2324399a2
hoangtongvu <hoangtongvu@gmail.com>
2025-06-02 07:59:14 +0700

    Add Attack only non-neutral & not-the-same faction

--- Assets/_Scripts/Systems/Simulation/Unit/Misc/FindTargetAndMoveToAttackSystem.cs
+++ Assets/_Scripts/Systems/Simulation/Unit/Misc/FindTargetAndMoveToAttackSystem.cs
@@ -38,6 +38,7 @@ namespace Systems.Simulation.Unit.Misc
                 .WithAll<
                     MoveCommandElement
                     , InteractingEntity
+                    , FactionIndex
                     , CanSetTargetJobScheduleTag>()
                 .WithAll<
                     IsArmedUnitTag>()
@@ -104,6 +105,7 @@ namespace Systems.Simulation.Unit.Misc
             {
                 PhysicsWorld = physicsWorld,
                 AttackConfigsMap = attackConfigsMap,
+                FactionIndexLookup = SystemAPI.GetComponentLookup<FactionIndex>(),
                 TargetEntityArray = targetEntityArray,
                 TargetPosArray = targetPosArray,
             }.ScheduleParallel(this.entityQuery, state.Dependency);
@@ -158,6 +160,9 @@ namespace Systems.Simulation.Unit.Misc
             [ReadOnly]
             public AttackConfigsMap AttackConfigsMap;
 
+            [ReadOnly]
+            public ComponentLookup<FactionIndex> FactionIndexLookup;
+
             public NativeArray<Entity> TargetEntityArray;
             public NativeArray<float3> TargetPosArray;
 
@@ -167,6 +172,7 @@ namespace Systems.Simulation.Unit.Misc
                 , in MoveCommandElement moveCommandElement
                 , in InteractingEntity interactingEntity
                 , in LocalTransform transform
+                , in FactionIndex factionIndex
                 , EnabledRefRW<CanSetTargetJobScheduleTag> canSetTargetJobScheduleTag
                 , Entity unitEntity
                 , [EntityIndexInQuery] int entityIndex)
@@ -203,6 +209,10 @@ namespace Systems.Simulation.Unit.Misc
                 for (int i = 0; i < length; i++)
                 {
                     var hit = hitList[i];
+
+                    this.FactionIndexLookup.TryGetComponent(hit.Entity, out var targetFactionIndex);
+                    if (targetFactionIndex == FactionIndex.Neutral) continue;
+                    if (factionIndex.Value == targetFactionIndex.Value) continue;
 
                     if (hit.Entity == unitEntity) continue;
                     if (smallestDistanceXZ <= hit.Distance) continue;
