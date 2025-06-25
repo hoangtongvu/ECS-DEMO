54076ef8fa710af5fb98d979a1b21035dc5afed3
hoangtongvu <hoangtongvu@gmail.com>
2025-06-25 07:33:15 +0700

    Add AddAndDisableComponent()

---
+++ Assets/_Scripts/Authoring/Utilities/Extensions/IBakerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+namespace Authoring.Utilities.Extensions
+{
+    public static class IBakerExtensions
+    {
+        public static void AddAndDisableComponent<T>(this IBaker baker, in Entity entity)
+            where T : struct, IEnableableComponent
+        {
+            baker.AddComponent<T>(entity);
+            baker.SetComponentEnabled<T>(entity, false);
+        }
+
+    }
+
+}
