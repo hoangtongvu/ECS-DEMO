c77235a40cea2c38ec2958bf1d613a8285025aa6
hoangtongvu <hoangtongvu@gmail.com>
2025-07-03 14:22:42 +0700

    Improve TransformAccessArr and TransformSyncSystem
    - Add indexPool Queue for reusing RemovedIndexes
    - Change to only add TransformAccessArrayIndex ICD for those that are moveable
    - Fix Invalid Index exception in TransformSyncSystem when any entity using this system was destroyed

--- Assets/_Scripts/Components/Misc/Presenter/TransformAccessArrayIndex.cs
+++ Assets/_Scripts/Components/Misc/Presenter/TransformAccessArrayIndex.cs
@@ -5,6 +5,10 @@ namespace Components.Misc.Presenter
     public struct TransformAccessArrayIndex : IComponentData
     {
         public int Value;
+        public static readonly TransformAccessArrayIndex Invalid = new()
+        {
+            Value = -1,
+        };
     }
 
 }
