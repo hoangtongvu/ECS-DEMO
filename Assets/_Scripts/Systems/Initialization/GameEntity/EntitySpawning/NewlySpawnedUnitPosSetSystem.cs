5960d4085aa7ceb021bb9746ef8ecbb96b53b6ac
hoangtongvu <hoangtongvu@gmail.com>
2025-07-04 15:21:00 +0700

    Refactor NewlySpawnedUnitPosSetSystem to use new CommandList

--- Assets/_Scripts/Systems/Initialization/GameEntity/EntitySpawning/NewlySpawnedUnitPosSetSystem.cs
+++ Assets/_Scripts/Systems/Initialization/GameEntity/EntitySpawning/NewlySpawnedUnitPosSetSystem.cs
@@ -1,11 +1,9 @@
 using Components.GameEntity.EntitySpawning;
 using Components.GameEntity.Misc;
-using Core.Utilities.Extensions;
 using Systems.Initialization.Misc;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace Systems.Initialization.GameEntity.EntitySpawning
@@ -15,15 +13,9 @@ namespace Systems.Initialization.GameEntity.EntitySpawning
     [BurstCompile]
     public partial struct NewlySpawnedUnitPosSetSystem : ISystem
     {
-        private Random rand;
-        private float2 tempVector;
-
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            this.rand = new Random(1);
-            this.tempVector = new(1, 1);
-
             var entityQuery = SystemAPI.QueryBuilder()
                 .WithAll<
                     NewlySpawnedTag
@@ -33,17 +25,18 @@ namespace Systems.Initialization.GameEntity.EntitySpawning
                 .Build();
 
             state.RequireForUpdate(entityQuery);
+            state.RequireForUpdate<SetPosWithinRadiusCommandList>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            var commandList = SystemAPI.GetSingleton<SetPosWithinRadiusCommandList>().Value;
             var entities = new NativeList<Entity>(10, Allocator.Temp);
 
-            foreach (var (spawnerEntityHolderRef, transformRef, entity) in SystemAPI
+            foreach (var (spawnerEntityHolderRef, entity) in SystemAPI
                 .Query<
-                    RefRO<SpawnerEntityHolder>
-                    , RefRW<LocalTransform>>()
+                    RefRO<SpawnerEntityHolder>>()
                 .WithAll<
                     NewlySpawnedTag
                     , NeedInitPosAroundSpawnerTag>()
@@ -53,20 +46,16 @@ namespace Systems.Initialization.GameEntity.EntitySpawning
                 var spawnerEntity = spawnerEntityHolderRef.ValueRO.Value;
                 if (spawnerEntity == Entity.Null) continue;
 
-                var spawnerTransform = SystemAPI.GetComponent<LocalTransform>(spawnerEntity);
-
-                transformRef.ValueRW.Position = this.GetRandomPositionInRadius(3, spawnerTransform.Position); // TODO: Find another way to get this value
+                commandList.Add(new()
+                {
+                    BaseEntity = entity,
+                    CenterPos = SystemAPI.GetComponent<LocalTransform>(spawnerEntity).Position,
+                    Radius = 3f,
+                });
             }
 
             state.EntityManager.RemoveComponent<NeedInitPosAroundSpawnerTag>(entities.AsArray());
 
-        }
-
-        private float3 GetRandomPositionInRadius(in float radius, in float3 centerPos)
-        {
-            // This is heavy work?? try to use job.
-            float2 distanceVector = math.normalize(this.rand.NextFloat2(-this.tempVector, this.tempVector)) * radius;
-            return centerPos.Add(x: distanceVector.x, z: distanceVector.y);
         }
 
     }
