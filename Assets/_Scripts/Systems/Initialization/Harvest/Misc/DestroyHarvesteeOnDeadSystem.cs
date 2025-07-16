b99e012a40ff65f2ef4997d980545c51a2fa0b59
hoangtongvu <hoangtongvu@gmail.com>
2025-07-16 16:39:06 +0700

    Add Free cells on Harvestee dead

---
+++ Assets/_Scripts/Systems/Initialization/Harvest/Misc/DestroyHarvesteeOnDeadSystem.cs
@@ -0,0 +1,52 @@
+using Components.GameEntity.Damage;
+using Components.GameEntity.Misc;
+using Components.GameEntity.Misc.EntityCleanup;
+using Components.Harvest;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Utilities.Extensions;
+
+namespace Systems.Initialization.Harvest.Misc
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [BurstCompile]
+    public partial struct DestroyHarvesteeOnDeadSystem : ISystem
+    {
+        private EntityQuery entityQuery;
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            entityQuery = SystemAPI.QueryBuilder()
+                .WithAll<
+                    HarvesteeTag>()
+                .WithDisabled<
+                    IsAliveTag>()
+                .Build();
+
+            state.RequireForUpdate(entityQuery);
+
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var entities = entityQuery.ToEntityArray(Allocator.Temp);
+            if (entities.Length == 0) return;
+
+            var em = state.EntityManager;
+
+            em.AddComponent<NeedDestroyEntityTag>(entities);
+            em.AddComponent<PendingCleanupEntityTag>(entities);
+            em.AddComponentData(entities, new PendingCleanupEntityTimer
+            {
+                TimeStamp = SystemAPI.Time.ElapsedTime,
+                DurationSeconds = new(0f),
+            });
+
+        }
+
+    }
+
+}
