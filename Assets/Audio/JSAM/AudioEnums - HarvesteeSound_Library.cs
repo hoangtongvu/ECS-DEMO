dd99ec67d1381251e8de4de4ea06088db477903d
hoangtongvu <hoangtongvu@gmail.com>
2025-07-31 07:45:19 +0700

    Add OnHitHarvestee SFXs

---
+++ Assets/Audio/JSAM/AudioEnums - HarvesteeSound_Library.cs
@@ -0,0 +1,11 @@
+namespace Audio.JSAM {
+    public enum HarvesteeSound_LibrarySounds {
+        OnHitTreeSound,
+        OnHitResourcePitSound,
+        OnHitBerryBushSound
+    }
+}
+namespace Audio.JSAM {
+    public enum HarvesteeSound_LibraryMusic {
+    }
+}
---
+++ Assets/_Scripts/Systems/Simulation/Harvest/Presenter/SFXPlayer/BaseSFXOnHitSystem.cs
@@ -0,0 +1,54 @@
+using Audio.JSAM;
+using Components.GameEntity.Damage;
+using Components.Harvest;
+using Core.Harvest;
+using JSAM;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Systems.Simulation.Harvest.Presenter.SFXPlayer
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public abstract partial class BaseSFXOnHitSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            var query = SystemAPI.QueryBuilder()
+                .WithAll<
+                    HarvesteeTag
+                    , HarvesteeProfileIdHolder
+                    , LocalTransform>()
+                .WithAll<
+                    NewlyTakeHitTag>()
+                .Build();
+
+            this.RequireForUpdate(query);
+        }
+
+        protected override void OnUpdate()
+        {
+            var harvesteeType = this.GetHarvesteeType();
+            var soundType = this.GetSoundType();
+
+            foreach (var (harvesteeProfileIdHolderRef, transformRef) in SystemAPI
+                .Query<
+                    RefRO<HarvesteeProfileIdHolder>
+                    , RefRO<LocalTransform>>()
+                .WithAll<
+                    HarvesteeTag>()
+                .WithAll<
+                    NewlyTakeHitTag>())
+            {
+                if (harvesteeProfileIdHolderRef.ValueRO.Value.HarvesteeType != harvesteeType) continue;
+                AudioManager.PlaySound(soundType, transformRef.ValueRO.Position);
+            }
+
+        }
+
+        protected abstract HarvesteeType GetHarvesteeType();
+
+        protected abstract HarvesteeSound_LibrarySounds GetSoundType();
+
+    }
+
+}
