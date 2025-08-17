9607aacda6f34b3fa526f3103736cc9d9b1d615f
hoangtongvu <hoangtongvu@gmail.com>
2025-08-17 16:53:24 +0700

    Apply PresentingActions on Units + remove old ones

--- Assets/_Scripts/Core/Unit/Presenter/AnimationPlayerOnMineStarted.cs
+++
@@ -1,9 +0,0 @@
-using Core.GameEntity.Presenter;
-using Core.Harvest.Messages;
-
-namespace Core.Unit.Presenter
-{
-    public class AnimationPlayerOnMineStarted : AnimationPlayerOnReactionStarted<OnMineStartedMessage>
-    {
-    }
-}
