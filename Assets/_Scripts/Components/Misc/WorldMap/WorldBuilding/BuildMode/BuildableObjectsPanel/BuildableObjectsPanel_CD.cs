2a5f16bbd22f7f7f1ef81ea5ffd2e8ea79651e69
hoangtongvu <hoangtongvu@gmail.com>
2025-08-28 21:03:41 +0700

    Add Components

---
+++ Assets/_Scripts/Components/Misc/WorldMap/WorldBuilding/BuildMode/BuildableObjectsPanel/BuildableObjectsPanel_CD.cs
@@ -0,0 +1,25 @@
+﻿using Core.UI.WorldMap.BuildableObjects.BuildableObjectsPanel;
+using Unity.Entities;
+
+namespace Components.Misc.WorldMap.WorldBuilding.BuildMode.BuildableObjectsPanel
+{
+    public struct BuildableObjectsPanel_CD
+    {
+        public struct Holder : IComponentData
+        {
+            public UnityObjectRef<BuildableObjectsPanelCtrl> Value;
+        }
+
+        public struct CanShow : IComponentData, IEnableableComponent
+        {
+        }
+
+        public struct CanUpdate : IComponentData, IEnableableComponent
+        {
+        }
+
+        public struct IsVisible : IComponentData, IEnableableComponent
+        {
+        }
+    }
+}
---
+++ Assets/_Scripts/Components/Misc/WorldMap/WorldBuilding/BuildMode/ExitBuildModeButton/ExitBuildModeButton_CD.cs
@@ -0,0 +1,25 @@
+﻿using Core.UI.WorldMap.BuildableObjects.ExitBuildModeButton;
+using Unity.Entities;
+
+namespace Components.Misc.WorldMap.WorldBuilding.BuildMode.ExitBuildModeButton
+{
+    public struct ExitBuildModeButton_CD
+    {
+        public struct Holder : IComponentData
+        {
+            public UnityObjectRef<ExitBuildModeButtonCtrl> Value;
+        }
+
+        public struct CanShow : IComponentData, IEnableableComponent
+        {
+        }
+
+        public struct CanUpdate : IComponentData, IEnableableComponent
+        {
+        }
+
+        public struct IsVisible : IComponentData, IEnableableComponent
+        {
+        }
+    }
+}
