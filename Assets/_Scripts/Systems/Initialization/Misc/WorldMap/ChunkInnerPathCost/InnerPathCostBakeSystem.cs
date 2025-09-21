d891dd85b06d96092b5385669eeaf24525af6bf0
hoangtongvu <hoangtongvu@gmail.com>
2025-09-21 15:12:22 +0700

    Add Job version for InnerPathCostBakeSystem

--- Assets/_Scripts/Systems/Initialization/Misc/WorldMap/ChunkInnerPathCost/InnerPathCostBakeSystem.cs
+++ Assets/_Scripts/Systems/Initialization/Misc/WorldMap/ChunkInnerPathCost/InnerPathCostBakeSystem.cs
@@ -11,6 +11,7 @@ using Utilities.Helpers;
 using Utilities.Helpers.Misc.WorldMap.ChunkInnerPathCost;
 using Core.Misc.WorldMap;
 using Utilities.Helpers.Misc.WorldMap;
+using Unity.Jobs;
 
 namespace Systems.Initialization.Misc.WorldMap.ChunkInnerPathCost
 {
@@ -42,7 +43,6 @@ namespace Systems.Initialization.Misc.WorldMap.ChunkInnerPathCost
                 {
                     Value = new(500, Allocator.Persistent),
                 });
-
         }
 
         [BurstCompile]
@@ -61,72 +61,131 @@ namespace Systems.Initialization.Misc.WorldMap.ChunkInnerPathCost
             var innerPathCostMap = SystemAPI.GetSingleton<InnerPathCostMap>();
             var highestExitCount = SystemAPI.GetSingleton<HighestExitCount>().Value;
 
+            // TODO: Find another way to get this value
+            // The cap ratio is around 2.5f, tested with some cases
+            const int capRatio = 3;
+            int innerPathCostMapCap = chunkExitsContainer.Value.Length * capRatio;
+
+            var localPathCostMap = new NativeParallelHashMap<InnerPathKey, float>(innerPathCostMapCap, Allocator.TempJob);
             innerPathCostMap.Value.Clear();
 
-            FinalCostComputer finalCostComputer = new()
+            state.Dependency = new PathCostsBakingJob
             {
                 CostMap = costMap,
                 CellPosRangeMap = cellPosRangeMap,
                 CellPositionsContainer = cellPositionsContainer,
-            };
+                ChunkExitIndexesContainer = chunkExitIndexesContainer,
+                ChunkExitsContainer = chunkExitsContainer,
+                ChunkIndexToExitIndexesMap = chunkIndexToExitIndexesMap,
+                HighestExitCount = highestExitCount,
+                LocalPathCostMap = localPathCostMap.AsParallelWriter(),
+            }.ScheduleParallel(chunkList.Value.Length, 64, state.Dependency);
 
-            var exits = new NativeList<ChunkExit>(highestExitCount, Allocator.Temp);
-            exits.AddReplicate(default, highestExitCount);
-
-            int chunkListLength = chunkList.Value.Length;
-            for (int chunkIndex = 0; chunkIndex < chunkListLength; chunkIndex++)
+            state.Dependency = new SyncLocalToGlobalInnerPathCostMap
             {
-                ChunkExitHelper.GetExitsFromChunk(
-                    in chunkIndexToExitIndexesMap
-                    , in chunkExitIndexesContainer
-                    , in chunkExitsContainer
-                    , chunkIndex
-                    , ref exits
-                    , out int exitCount);
+                LocalPathCostMap = localPathCostMap,
+                InnerPathCostMap = innerPathCostMap,
+            }.Schedule(state.Dependency);
 
-                this.BakeChunkInnerPathCosts(in costMap, in innerPathCostMap, ref finalCostComputer, chunkIndex, in exits, exitCount);
+            state.Dependency = localPathCostMap.Dispose(state.Dependency);
+        }
+
+        [BurstCompile]
+        private struct PathCostsBakingJob : IJobParallelForBatch
+        {
+            [ReadOnly] public WorldTileCostMap CostMap;
+            [ReadOnly] public ChunkIndexToExitIndexesMap ChunkIndexToExitIndexesMap;
+            [ReadOnly] public ChunkExitIndexesContainer ChunkExitIndexesContainer;
+            [ReadOnly] public ChunkExitsContainer ChunkExitsContainer;
+            [ReadOnly] public int HighestExitCount;
+
+            [ReadOnly] public CellPosRangeMap CellPosRangeMap;
+            [ReadOnly] public CellPositionsContainer CellPositionsContainer;
+
+            public NativeParallelHashMap<InnerPathKey, float>.ParallelWriter LocalPathCostMap;
+
+            [BurstCompile]
+            public void Execute(int startIndex, int count)
+            {
+                int upperBound = startIndex + count;
+
+                PathCostComputer costComputer = new()
+                {
+                    CostMap = this.CostMap,
+                    InputCellPosRangeMap = this.CellPosRangeMap,
+                    InputCellPositionsContainer = this.CellPositionsContainer,
+                    LocalCachedLines = new(20, Allocator.Temp),
+                };
+
+                var exits = new NativeList<ChunkExit>(this.HighestExitCount, Allocator.Temp);
+                exits.AddReplicate(default, this.HighestExitCount);
+
+                for (int chunkIndex = startIndex; chunkIndex < upperBound; chunkIndex++)
+                {
+                    ChunkExitHelper.GetExitsFromChunk(
+                        in this.ChunkIndexToExitIndexesMap
+                        , in this.ChunkExitIndexesContainer
+                        , in this.ChunkExitsContainer
+                        , chunkIndex
+                        , ref exits
+                        , out int exitCount);
+
+                    this.BakeChunkInnerPathCosts(ref costComputer, chunkIndex, in exits, exitCount);
+                }
+
+            }
+
+            [BurstCompile]
+            private void BakeChunkInnerPathCosts(
+                ref PathCostComputer costComputer
+                , int chunkIndex
+                , in NativeList<ChunkExit> exits
+                , int exitCount)
+            {
+                for (int j = 0; j < exitCount; j++)
+                {
+                    ChunkExitHelper.GetUnsafeInnerCellFromExit(in this.CostMap, exits[j], chunkIndex, out _, out int cell0MapIndex);
+                    int2 pos0 = WorldMapHelper.MapIndexToGridPos(this.CostMap.Width, in this.CostMap.Offset, cell0MapIndex);
+
+                    for (int k = j + 1; k < exitCount; k++)
+                    {
+                        ChunkExitHelper.GetUnsafeInnerCellFromExit(in this.CostMap, exits[k], chunkIndex, out _, out int cell1MapIndex);
+
+                        bool canSkipPathWithCostOfZero = cell0MapIndex == cell1MapIndex;
+                        if (canSkipPathWithCostOfZero) continue;
+
+                        int2 pos1 = WorldMapHelper.MapIndexToGridPos(this.CostMap.Width, in this.CostMap.Offset, cell1MapIndex);
+
+                        costComputer.Pos0 = pos0;
+                        costComputer.Pos1 = pos1;
+
+                        float finalCost = costComputer.GetCost();
+
+                        int firstCellMapIndex = cell0MapIndex;
+                        int secondCellMapIndex = cell1MapIndex;
+
+                        this.LocalPathCostMap.TryAdd(new InnerPathKey(firstCellMapIndex, secondCellMapIndex), finalCost);
+                    }
+                }
 
             }
 
         }
 
         [BurstCompile]
-        private void BakeChunkInnerPathCosts(
-            in WorldTileCostMap costMap
-            , in InnerPathCostMap innerPathCostMap
-            , ref FinalCostComputer finalCostComputer
-            , int chunkIndex
-            , in NativeList<ChunkExit> exits
-            , int exitCount)
+        private struct SyncLocalToGlobalInnerPathCostMap : IJob
         {
-            for (int j = 0; j < exitCount; j++)
+            [ReadOnly] public NativeParallelHashMap<InnerPathKey, float> LocalPathCostMap;
+            [WriteOnly] public InnerPathCostMap InnerPathCostMap;
+
+            [BurstCompile]
+            public void Execute()
             {
-                ChunkExitHelper.GetUnsafeInnerCellFromExit(in costMap, exits[j], chunkIndex, out _, out int cell0MapIndex);
-                int2 pos0 = WorldMapHelper.MapIndexToGridPos(costMap.Width, in costMap.Offset, cell0MapIndex);
-
-                for (int k = j + 1; k < exitCount; k++)
+                foreach (var keyValue in this.LocalPathCostMap)
                 {
-                    ChunkExitHelper.GetUnsafeInnerCellFromExit(in costMap, exits[k], chunkIndex, out _, out int cell1MapIndex);
-
-                    bool canSkipPathWithCostOfZero = cell0MapIndex == cell1MapIndex;
-                    if (canSkipPathWithCostOfZero) continue;
-
-                    int2 pos1 = WorldMapHelper.MapIndexToGridPos(costMap.Width, in costMap.Offset, cell1MapIndex);
-
-                    finalCostComputer.Pos0 = pos0;
-                    finalCostComputer.Pos1 = pos1;
-
-                    float finalCost = finalCostComputer.GetCost();
-
-                    int firstCellMapIndex = cell0MapIndex;
-                    int secondCellMapIndex = cell1MapIndex;
-
-                    innerPathCostMap.Value.TryAdd(new InnerPathKey(firstCellMapIndex, secondCellMapIndex), finalCost);
-
+                    this.InnerPathCostMap.Value.TryAdd(keyValue.Key, keyValue.Value);
                 }
-
             }
-
         }
 
     }
